4f4de0810e9a00e56fbacb345332610d28b38de0
rebwon <msolo021015@gmail.com>
2019-03-18 20:13:30 +0900

    2019/03/18 Queue Study

--- src/main/java/ko/maeng/queue/QueueTest.java
+++ src/main/java/ko/maeng/queue/QueueTest.java
@@ -1,9 +1,22 @@
 package ko.maeng.queue;
 
+import java.util.Optional;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class QueueTest {
+    //Queue 먼저 들어온 것이 먼저 나가는 FIFO(First In First Out) 구조를 가진다.
     public static void main(String[] args) {
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.offer(6);
+        queue.add(5);
+        queue.add(4);
+
+        System.out.println(queue.peek());
+
+        while(!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
 
     }
 }
